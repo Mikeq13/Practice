d1bac804b8c2fc760e9f21a0bc8ebc78822dead3
Mike <13jmmq@gmail.com>
2018-03-05 21:41:23 -0700

    setup new app

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+
+  get 'pages/home'
+
+  get 'pages/contact'
+
+  get 'pages/about'
+
+  resources :portfolios
+  resources :blogs
+
+  root to: "pages#home"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
